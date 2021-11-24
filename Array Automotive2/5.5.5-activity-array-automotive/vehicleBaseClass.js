15d4fd66d341b6f90f5ce045101ca288c54ea154
Andrew Tapalla <89113477+andrewtapalla@users.noreply.github.com>
2021-11-24 09:29:37 -0800

    Add files via upload

---
+++ Array Automotive2/5.5.5-activity-array-automotive/vehicleBaseClass.js
@@ -0,0 +1,91 @@
+class Vehicle {
+    constructor(make, model, year, color, mileage) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.passenger = 0;
+        this.speed = 0;
+        this.mileage = mileage;
+        this.started = false;
+        this.numberOfWheels = 0;
+    }
+
+    start() {
+        if (this.fuel > 0) {
+            return this.started = true;
+            console.log("Engine Started");
+        } else {
+            return this.started = false;
+            console.log("Cannot start engine.");
+        }
+    }
+    accelerate() {
+        if (this.started) {
+            if (this.fuel > 0) {
+                console.log(this.speed += 1);
+                this.fuel = this.fuel - 1;
+            } else {
+                console.log('Out of fuel.");
+                this.stop();
+            }
+        } else {
+            alert("Please start engine.");
+        }
+    }
+    decelerate() {
+        if (this.started) {
+            if (this.fuel > 0) {
+                if (this.speed > 0) {
+                    console.log(this.speed -= 1);
+                    this.fuel = this.fuel - 1;
+                } else {
+                    console.log(this + " has stopped moving");
+                    this.fuel = this.fuel - 1;
+                }
+            } else {
+                console.log("Out of fuel.");
+                this.stop();
+            }
+        } else {
+            alert("You need to start the engine first.");
+        }
+    }
+    stop() {
+        this.started = false;
+    }
+
+    drive() {
+        accelerate();
+    }
+    brake() {
+        decelerate();
+    }
+
+    autoPark()
+    {
+
+    }
+
+    autoDrive()
+    {
+      
+    }
+
+    typeOfVehicle(wheels) {
+        if (this.numberOfWheels == 8 && 8 == wheels) {
+            console.log(this.model + " " + this.make + " is a truck");
+        } else if (this.numberOfWheels == 4 && 4 == wheels) {
+            console.log(this.model + " " + this.make + " is a car");
+        } else if (this.numberOfWheels == 2 && 2 == wheels) {
+            console.log(this.model + " " + this.make + " is a motorcycle");
+        } else {
+            console.log("Unknown vehicle");
+        }
+    }
+}
+
+
+module.exports = {
+    Vehicle
+}
